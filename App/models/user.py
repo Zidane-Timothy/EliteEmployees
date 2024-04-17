876c325ebb1548ca7cdc1e108ec99907c8318679
Zidane-Timothy <130504405+Zidane-Timothy@users.noreply.github.com>
2024-04-16 22:45:13 -0400

    Revert "Merge branch 'sequence'"
    This reverts commit 62251542ee4d3a4dceea6b643c4a433f5e3ff848, reversing
    changes made to 17a71a5923f920403ae95405ccd588ac41b899ec.

--- App/models/user.py
+++ App/models/user.py
@@ -1,9 +1,8 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, date
+from datetime import datetime
 from sqlalchemy.sql.expression import func
 from App.database import db
-import random
 
 # db = SQLAlchemy()
 
@@ -52,7 +51,7 @@ class User(db.Model):
     def set_password(self, password):
         """Create hashed password."""
         self.password = generate_password_hash(password)
-
+    
     def check_password(self, password):
         """Check hashed password."""
         return check_password_hash(self.password, password)
@@ -68,50 +67,53 @@ class User(db.Model):
 
     def new_game(self, game_id):
         game = UserGame.q.filter_by(user_id=self.id, game_id=game_id).first()
+      
+
 
 class UserGame(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    tries = db.Column(db.Integer, nullable=False)
-    status = db.Column(db.String, nullable=False)
+  id = db.Column(db.Integer, primary_key = True)
+  user_id = db.Column(db.Integer,db.ForeignKey('user.id'), nullable = False)
+  tries = db.Column(db.Integer, nullable = False)
+  status = db.Column(db.String, nullable = False)
 
-    def get_json(self):
-        return {
-            'id': self.id,
-            'user_id': self.user_id,
-            'tries': self.tries,
-            'status': self.status
-        }
+  def get_json(self):
+    return {
+      'id': self.id,
+      'user_id': self.user_id,
+      'tries': self.tries,
+      'status': self.status
+    }
 
-    def __init__(self, user_id, tries, status):
-        self.user_id = user_id
-        self.tries = tries
-        self.status = status
+  def __init__(self, user_id, tries, status):
+    self.user_id = user_id
+    self.tries = tries
+    self.status = status
 
 class Game(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    sequence = db.Column(db.String(4), nullable=False)
-    last_update = db.Column(db.Date, nullable=False, default=datetime.utcnow().date())  # Changed to store only date
+  id = db.Column(db.Integer, primary_key = True)
+  sequence = db.Column(db.String(4), nullable = False)
+  last_update = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+  
 
-    def get_json(self):
-        return {
-            'id': self.id,
-            'sequence': self.sequence,
-            'last_update': self.last_update
-        }
+  def get_json(self):
+    return {
+      'id': self.id,
+      'sequence': self.sequence,
+      'last_update': self.last_update
+    }
 
   def __init__(self, sequence, last_update):
     self.sequence = sequence
     self.last_update = last_update
-
-  def generate_sequence():
-    sequence = ''
-    for i in range(4):
-      sequence += str(random.randint(0,9))
-    return sequence
   
-  # def generate_unique_sequence():
-  #   digits = list(range(10))  # Create a list of digits from 0 to 9
-  #   random.shuffle(digits)    # Shuffle the list randomly
-  #   unique_sequence = ''.join(map(str, digits[:4]))  # Take the first 4 digits
-  #   return unique_sequence
+  def generate_sequence():
+    digits = list(range(10))  # Create a list of digits from 0 to 9
+    random.shuffle(digits)    # Shuffle the list randomly
+    unique_sequence = ''.join(map(str, digits[:4]))  # Take the first 4 digits
+    return unique_sequence
+
+  # def generate_sequence():
+  #   sequence = ''
+  #   for i in range(4):
+  #     sequence += str(random.randint(0,9))
+  #   return sequence
